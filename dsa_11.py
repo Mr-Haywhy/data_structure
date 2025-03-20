889fa8ab1ef8753ada009c064d801fa6c0ec7825
Mr Haywhy <tosinblessing.akinyemi@gmail.com>
2025-03-20 17:56:12 +0100

    Linked list

--- dsa_11.py
+++ dsa_11.py
@@ -70,3 +70,95 @@
                 # node3 = Node(14)
             # # link the second and third nodes
                 # node2.next = node3
+# 
+# 
+    # The head is the first node in the linked list.
+    # Each node is connected to each other.
+    # The last node points to None.
+
+
+# create the Node class
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# create the LinkedList class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # method to create a linked list
+    # a linked list of three nodes:
+    # 80->9->14
+    def create_linked_list(self):
+
+        # create and link nodes
+        node1 = Node(80)
+        self.head = node1
+
+        node2 = Node(9)
+        node1.next = node2
+
+        node3 = Node(14)
+        node2.next = node3
+
+linked_list = LinkedList()
+
+# create linked list
+linked_list.create_linked_list()
+
+
+
+#..............TRAVERSE THROUGH THE LINKED LIST.............#
+# We will now access each node one by one in a linked list.
+# NOTE: We cannot access elements in a linked list directly using an index, as we would with a Python list. That's why we need to traverse the linked list to access its elements.
+#.....How to traverse through a linked list?.....#
+# We will start our traversal from the head node. Then, we will move toward the next element and repeat this process until the next element points to None.
+# 
+# 1. Assign head to the current variable.
+        # get the address to the head of the list
+        # current = self.head
+# 
+# 2. Iterate until the current variable is None.
+        # iterate until current is None
+        # current is updated to the next node in each iteration
+
+        # while current:
+        #     current = current.next
+# 
+# If current.next is None, we know for sure that it is the last node.
+# 
+# Let's add a method to our LinkedList class that will be used for traversal.
+# 
+# 
+# method to traverse and print a linked list
+def traverse_linked_list(self):
+    current = self.head
+    while current:
+        print(f"{current.data}", end="->")
+        current = current.next
+    print(None)
+
+
+
+#........Count Number of Node..........#
+# method to count elements in a linked list
+def count_node(self):
+    current = self.head
+    # initialize count to zero
+    count = 0
+    while current:
+        count += 1
+        current = current.next
+    return count
+
+
+#.............Sum of Node............#
+def calculate_node_sum(self):
+    current = self.head
+    sum = 0
+    while current:
+        sum = current.data
+        current = current.next
+    return sum
