c4f225e6a0513d4fd2afcf42f6b60913641e15ba
Mr Haywhy <tosinblessing.akinyemi@gmail.com>
2025-04-04 15:25:58 +0100

    Hashing

--- hashing.py
+++ hashing.py
@@ -128,3 +128,33 @@
 # After we perform these steps, we will get a hash table where:
 #     * the elements represent the keys,
 #     * their indexes represent the hash values.
+
+class HashData:
+    # initialize a list of 10 items
+    def __init__(self):
+        self.table = [None] * 10
+
+    # compute hash
+    def hash_function(self, key):
+        return key % 10
+
+    # insert data to hash table
+    def put(self, key):
+        hash_value = self.hash_function(key)
+        self.table[hash_value] = key
+
+    # display hash table
+    def display(self):
+        for hash_value, key in enumerate(self.table):
+            print(f"{hash_value}: {key}")    
+            
+# keys
+keys = [1, 300, 209, 117, 12]
+
+hash1 = HashData()
+
+# apply hash function to each key
+for key in keys:
+    hash1.put(key)
+
+hash1.display()
