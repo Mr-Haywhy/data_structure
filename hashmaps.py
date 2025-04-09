957662a3eb84f63d8b85813abd90f05f80f57190
Mr Haywhy <tosinblessing.akinyemi@gmail.com>
2025-04-09 16:30:48 +0100

    Hashmaps

--- hashmaps.py
+++ hashmaps.py
@@ -60,3 +60,73 @@
     # 3 Add a new node to the end of the list if the key is not already present.
 
 # Say we have to insert a new key-value pair, {5, "Mira"}, into a hash table of size 13.
+
+
+#...............Working: Inserting Into a Hashmap
+# Let's proceed with inserting {5, "Mira"} into our hash table of size 13.
+
+# Step 1: Hash the key to get the hash table index.
+
+# We'll first get the hash of the key 5. For this example,
+
+# We'll use a modular hash function, i.e., key % table_size.
+# So, we get the hash value of 5 (5 % 13).
+# Steps 2 and 3: Insert the key-value pair.
+
+# Now, we'll use the hash value 5 as an index in the hash table.
+
+# Currently, there's no linked list associated with this index. So, we insert {5, "Mira"} into a new linked list and connect it to the hash table at index 5.
+
+            # Hash Table
+                # 1
+# {5,"MIra"}    # 2
+    #   |       # 3
+    #   |       # 4
+#[5 % 13 = 5]   # 5 ----->{5,"MIra"}
+                # 6              
+                # 7
+                # 8
+                # 9
+                # 10
+                # 11
+                # 12
+                # 13
+
+
+# Next, let's see how we deal with collisions by adding a new key-value pair at the same index.
+
+#..............Working: Dealing with Collisions
+# Now, we'll insert a new key-value pair: {"18", "Paul"}.
+
+# 1. Get the hash of the key to get the hash table index.
+    # Using the modular hash function, we get the hash value 5 (18 % 13). So, we'll store this key-value at index 5.
+
+# 2. Traverse the linked list associated with the index.
+    # The key-value pair {5, "Mira"} is already present at index 5 of the hash table. So, a collision occurs.
+
+# 3. Resolve the collision by adding the pair as a new node in the linked list.
+    # To resolve the collision, we add {"18", "Paul"} as the next node (after {5, "Mira"}) to the list with the key 5.
+
+
+            # Hash Table
+                # 1
+#{18,"Paul"}    # 2
+    #   |       # 3
+    #   |       # 4
+#[18 % 13 = 5]  # 5 ----->{5,"MIra"}---->{18, "Paul"}
+                # 6              
+                # 7
+                # 8
+                # 9
+                # 10
+                # 11
+                # 12
+                # 13
+
+
+#..........Working: Fetching a Value From a Hashmap............
+# To fetch a value from the hashmap,
+    # 1 We hash the key using the hash function to find the index in the hash table.
+    # 2 Traverse the linked list at the resulting index until you find the key.
+    # 3 Return the value associated with the key.
+# Next, we'll demonstrate this operation with an example.
