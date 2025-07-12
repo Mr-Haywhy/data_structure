98782d6b2bef40fb6487cc4bb85223098566971f
Mr Haywhy <tosinblessing.akinyemi@gmail.com>
2025-07-12 14:59:57 +0100

    graph_tavasal_with_bfs_algorithm

---
+++ Graph_data_structure/graph_tavasal_with_bfs_algorithm.py
@@ -0,0 +1,14 @@
+"""
+Introduction
+Breadth-First Search is a graph traversal algorithm that explores the graph layer by layer.
+
+Real-Life Analogy
+
+When you post a photo or a status update on social media, your immediate friends or followers see it first.
+
+When they like, share, or comment on it, their friends (who might not be directly connected to you) see it, and then their friends, and so on. The post reaches people in layers of closeness to you.
+
+The BFS algorithm works in a similar manner. It starts from a source vertex and explores all its adjacent vertices at the current depth before moving on to the vertices at the next depth level.
+
+
+"""
