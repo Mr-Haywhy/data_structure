01b58ee2041a94c426e5cdb8d92838fdf2778d11
Mr Haywhy <tosinblessing.akinyemi@gmail.com>
2025-07-15 11:23:59 +0100

    graph based algorithm

---
+++ Graph_data_structure/graph_based_algorithm.py
@@ -0,0 +1,22 @@
+'''
+~ Introduction
+Graph-based algorithms provide us with techniques for working with graphs to gather valuable understandings and solve complex problems effectively.
+
+These algorithms help us perform various tasks such as:
+    * determining the quickest route between two points
+    * finding connected components in a network
+    * discovering trends in data
+    * solving mazes
+    * solving practical exercises in artificial intelligence and machine learning.
+
+~ Graph Based Algorithm
+In this chapter, you will learn about the following graph-based algorithms:
+    * Topological Sorting
+    * Dijkstra's Algorithm
+    * Bellman Ford's Algorithm
+    * Ford Fulkerson Algorithm
+    * Kruskal's Algorithm
+    * Prim's Algorithm
+
+
+'''
